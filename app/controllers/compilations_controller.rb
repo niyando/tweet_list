c37d00c8b656a4c70244fe9bd5d6dba085444604
Nirav <niyando@gmail.com>
2017-09-02 12:40:59 +0530

    logic to connect with twitter

---
+++ app/controllers/compilations_controller.rb
@@ -0,0 +1,6 @@
+class CompilationsController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'compilations#index'
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
