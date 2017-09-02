3d5fc6c26c0026474cfb3931852faaa077ce99bf
Nirav <niyando@gmail.com>
2017-09-02 15:30:47 +0530

    compilations controller and routes

--- app/controllers/compilations_controller.rb
+++ app/controllers/compilations_controller.rb
@@ -1,6 +1,40 @@
 class CompilationsController < ApplicationController
 
+  before_filter :authorize, except: [:index, :show]
+
+  before_filter :set_compilation, except: [:index, :new, :create]
+  before_filter :can_modify_compilation?, except: [:index, :new, :create]
+
   def index
+    @compilations = Compliations.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show;end
+
+  def edit;end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_compilation
+    @compilation = Compilation.find params[:id]
+  end
+
+  def can_modify_compilation?
+    unless current_user.id == @compilation.user.id
+      # redirect and show error
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :compilations
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
