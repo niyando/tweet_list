b0e50b88c876c13f849d0fd7fbc2649950766f66
Nirav <niyando@gmail.com>
2017-09-10 11:54:11 +0530

    validation fix

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :compilation
   validates_presence_of :link
-  validates :link, format:{with: /https?:\/\/twitter\.com\/\w+/i, message: :format} 
+  validates :link, format:{with: /https?:\/\/twitter\.com\/\w+\/status\/\d+/i, message: :format} 
   
 end
